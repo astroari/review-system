bfb55f152d0a49a44f93984dac01414acdf70c16
Arina <bagdaarina@gmail.com>
2024-10-18 16:04:27 +0500

    Add review model

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    order_id = models.CharField(max_length=6, unique=True)
+    # rating = models.IntegerField(max_length=1)
+    review = models.TextField(blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
