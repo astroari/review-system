d00a5b27b1104a0a61672ced56c3544639c17469
Arina <bagdaarina@gmail.com>
2024-10-21 10:08:05 +0500

    Star selection code

--- review_system/urls.py
+++ review_system/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rating.views import main_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view, name='main-view'),
 ]
